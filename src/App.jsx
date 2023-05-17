21a5ae812d431b363350f79ca3b17946bd892f24
Jairo-Digitt-Practicas <121839622+Jairo-Digitt-Practicas@users.noreply.github.com>
2023-05-16 19:52:12 -0600

    Cambios semana 3 de Mayo

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,45 @@
-import Cat from "./components/Cat";
-import Controlado from "./components/Controlado";
+import { useState } from "react";
+import Formulario from "./components/Formulario";
+import Todos from "./components/Todos"
+
+const initialStateTodos = [
+  {
+    id: 1,
+    title: "Todo #01",
+    description: "Description #01",
+    state: true,
+    priority: true
+  },
+  {
+    id: 2,
+    title: "Todo #02",
+    description: "Description #02",
+    state: false,
+    priority: false
+  },
+  {
+    id: 3,
+    title: "Todo #03",
+    description: "Description #03",
+    state: false,
+    priority: true
+  }
+]
 
 const App = () => {
+
+  const [todos, setTodos] = useState(initialStateTodos);
+
+  const addTodo = todo => {
+    setTodos([...todos, todo])
+  }
+
   return (
-    <div className="container">
-      <h1>Formularios</h1>
-      <Controlado />
+    <div className="container mb-2">
+      <h1 className="my-5">Formularios</h1>
+      <Formulario addTodo={addTodo} />
+      <Todos todos={todos} />
     </div>
   );
 };
-
 export default App;
--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,6 +1,7 @@
+import Swal from 'sweetalert2'
 import { useState } from "react";
 
-const Controlado = () => {
+const Formulario = ({ addTodo }) => {
 
     const [todo, setTodo] = useState({
         title: "Todo #01",
@@ -13,8 +14,28 @@ const Controlado = () => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(title, description, state);
 
+        if (!title.trim() || !description.trim()) {
+            return Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'Titulo y descriptión obligatorios!',
+            })
+        }
+
+        addTodo({
+            id: Date.now(),
+            ...todo,
+            state: state === "completado"
+        })
+
+        Swal.fire({
+            position: 'center',
+            icon: 'success',
+            title: 'Todo agregado correctamente',
+            showConfirmButton: false,
+            timer: 1500
+        })
     };
 
     const handleChange = e => {
@@ -76,10 +97,10 @@ const Controlado = () => {
             <button
                 type="submit"
                 className="btn btn-primary">
-                Procesar
+                Agregar todo
             </button>
         </form >
     )
 };
 
-export default Controlado;
+export default Formulario;
